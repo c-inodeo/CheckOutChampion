73ddfe9e4e1e804193eb88003ea907680ead2b1c
Claire <Claire@LAPTOP-SR6DU3OK>
2024-07-02 11:39:41 +0800

    Added Total for cart products

--- CheckOutChampionWeb/Areas/Customer/Controllers/CartController.cs
+++ CheckOutChampionWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using CheckOutChampion.DataAccess.Repository.IRepository;
 using CheckOutChampion.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
@@ -21,9 +22,10 @@ namespace CheckOutChampionWeb.Areas.Customer.Controllers
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var cartItems = _unitOfWork.Cart.GetAll(c => c.UserId == userId, includeProperties: "Product").ToList();
+
             return View(cartItems);
         }
-
+        [HttpPost]
         public IActionResult AddToCart(int productId, int quantity, bool isIncrement)
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -61,6 +63,9 @@ namespace CheckOutChampionWeb.Areas.Customer.Controllers
 
                 // Get the updated list of cart items for the current user
                 var cartItems = _unitOfWork.Cart.GetAll(c => c.UserId == userId, includeProperties: "Product").ToList();
+                TempData["success"] = "Updated cart!";
+                ViewBag.TotalPrice = GetTotalPrice();
+
                 return View("Index", cartItems);
             }
             else
@@ -68,6 +73,31 @@ namespace CheckOutChampionWeb.Areas.Customer.Controllers
                 return RedirectToAction("Login", "Account", new { area = "Identity" });
             }
         }
+        [HttpPost]
+        public IActionResult RemoveToCart(int cartId)
+        {
+            var itemToBeDeleted = _unitOfWork.Cart.Get(u=> u.CartId ==  cartId);
 
+            if (itemToBeDeleted != null)
+            {
+                _unitOfWork.Cart.Remove(itemToBeDeleted);
+                _unitOfWork.Save();
+                TempData["success"] = "Item deleted successfully!";
+                return RedirectToAction();
+            }
+            else 
+            {
+                return NotFound();
+            }
+
+        }
+        public double GetTotalPrice() 
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var cartItems = _unitOfWork.Cart.GetAll(c => c.UserId == userId, includeProperties: "Product").ToList();
+            double totalPrice = cartItems.Sum(item => item.Quantity * item.Product.Price);
+            
+            return totalPrice;
+        }
     }
 }
